d3e5f2fd2fa0f310be2863acf27478f2311eb92d
Aric <aric@coolheatcomfort.ca>
2020-02-05 14:42:42 -0500

    Alpha 1.1.2.3

--- CRMDesktop/Pages/Customers/Quote_Page.xaml.cs
+++ CRMDesktop/Pages/Customers/Quote_Page.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CRMDesktop.Pages.Customers
 {
@@ -35,7 +25,6 @@ namespace CRMDesktop.Pages.Customers
             InitializeComponent();
             fillPriceGuideComboBox();
             searchCustomers();
-            //populateFileList();
         }
         public void searchCustomers()
         {
